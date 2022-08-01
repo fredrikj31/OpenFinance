f55f49d1f4d69f1b6475795f1fa40668f4bcb8e0
Fredrik Johansen <fredrik1206@gmail.com>
2022-08-01 23:32:49 +0200

    Tried adding a logo

--- public/main.js
+++ public/main.js
@@ -7,8 +7,9 @@ function createWindow() {
 		width: 800,
 		height: 600,
 		webPreferences: {
-			preload: path.join(__dirname, 'preload.js')
+			preload: path.join(__dirname, 'preload.js'),
 		},
+		icon: path.join(__dirname + './assets/logo.png'),
 		show: false
 	});
 
